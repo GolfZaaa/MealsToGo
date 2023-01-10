fac96e02ed6b4500ff8baf98dfd792ef7573f93c
GolfZaaa <you@example.com>
2023-01-10 13:58:12 +0700

    Card

---
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { StyleSheet, View, SafeAreaView, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+export const RestaurantsScreen = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const onChangeSearch = (query) => setSearchQuery(query);
+
+  return (
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={styles.search}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+        </View>
+        <View style={styles.list}>
+        <RestaurantInfoCard/>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    search: {
+      padding: 16,
+      backgroundColor: "blanchedalmond",
+    },
+    list: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: "beige",
+      },
+  });
